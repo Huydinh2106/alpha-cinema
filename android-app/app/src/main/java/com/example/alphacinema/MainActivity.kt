e1abeff7fc2e89ba16c091609da119c62b165547
Huydinh2106 <huyd210625@gmail.com>
2026-03-29 15:00:08 +0700

    update UI

--- android-app/app/src/main/java/com/example/alphacinema/MainActivity.kt
+++ android-app/app/src/main/java/com/example/alphacinema/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.alphacinema
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -14,7 +13,7 @@ import com.example.alphacinema.ui.theme.AlphaCinemaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             AlphaCinemaTheme {
                 AppScreen(modifier = Modifier.fillMaxSize())
