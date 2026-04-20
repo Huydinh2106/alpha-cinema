a525f41a685d32bae90a3fd2efe78ff753ca7034
Huydinh2106 <huyd210625@gmail.com>
2026-04-21 00:09:40 +0700

    update thong bao

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.messaging)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
 
     // Credential Manager (Google Sign-In)
