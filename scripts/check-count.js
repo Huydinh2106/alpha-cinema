ddc019559f90cea64b0e97dca28dad41c2c974fa
Huydinh2106 <huyd210625@gmail.com>
2026-05-21 22:31:34 +0700

    Remove unused utility scripts

--- scripts/check-count.js
+++
@@ -1,8 +0,0 @@
-const { db } = require('./firebaseAdmin');
-
-async function checkCount() {
-    const res = await db.collection('movies').where('title', '>=', 'Phòng Thí Nghiệm').where('title', '<=', 'Phòng Thí Nghiệm\uf8ff').get();
-    res.docs.forEach(d => console.log('ID:', d.id, 'TITLE:', d.data().title));
-    process.exit(0);
-}
-checkCount();
